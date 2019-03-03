ce907d474339b70e0a91c93f1d2888666ca338f3
sunjie <15251800012@163.com>
2019-03-03 17:18:51 +0800

    springboot 项目初始化

--- src/main/java/com/pet/shop/controller/IndexController.java
+++ src/main/java/com/pet/shop/controller/IndexController.java
@@ -1,13 +1,16 @@
 package com.pet.shop.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class IndexController {
 
     @RequestMapping("/")
     public String  hello(){
+        log.info("准备进入首页！");
         return "index";
     }
 }
