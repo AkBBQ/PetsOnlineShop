dcf1cc594502b018660d0425d87b1ac243f2ef17
sunjie <15251800012@163.com>
2019-03-27 09:24:57 +0800

    登录功能

--- src/main/java/com/pet/shop/mapper/userMapper.java
+++ src/main/java/com/pet/shop/mapper/userMapper.java
@@ -4,8 +4,18 @@ import com.pet.shop.pojo.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+/**
+ * 用户Mapper
+ *
+ * @author 昂太 (angtai@maihaoche.com)
+ */
 @Mapper
-public interface userMapper {
-    @Select("select * from user")
-    public User login();
+public interface UserMapper {
+    /**
+     * 用户登录
+     * @param user
+     * @return
+     */
+    @Select("select * from user where phone =#{phone}")
+    User login(User user);
 }
